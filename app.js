f7a7f2956f5ee7a7a04f205277f0ab2b86e3f8eb
vrickey123 <vince.rickey@gmail.com>
2018-08-12 21:38:00 -0400

    Added frame for About container.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import {MDCTopAppBar} from '@material/top-app-bar/index';
-import {MDCToolbar, MDCToolbarFoundation} from '@material/toolbar';
 import {MDCTabBar} from '@material/tab-bar';
+import {MDCTab} from '@material/tab';
 
 // Top App Bar
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarElement);
 
-const toolbar = new MDCToolbar(document.querySelector('.mdc-toolbar'));
-
 const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
+
+const tab = new MDCTab(document.querySelector('.mdc-tab'));
