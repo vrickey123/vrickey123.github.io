84e5a37f7ebeaac756d2d2313e2445723d11bf8a
Vince Rickey <vince.rickey@gmail.com>
2020-11-27 23:43:21 -0500

    Created a components directory and extracted TopNavBar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,6 @@
-import { Container, Navbar, Row, Col, Nav, NavLink } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import TopNavBar from './components/TopNavBar'
 import './App.css';
-
-const TopNavBar = (props) => (
-  <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#vincerickey">Vince Rickey</Navbar.Brand>
-    <Nav classname="sections">
-      <NavLink href="#wsj">WSJ</NavLink>
-      <NavLink href="opensource">Open Source</NavLink>
-      <NavLink href="#wsj">Foreign Languages</NavLink>
-    </Nav>
-    <Nav>
-      <NavLink href="#resume">Resume</NavLink>
-    </Nav>
-  </Navbar>
-);
 
 function App() {
   return (
---
+++ src/components/TopNavBar.js
@@ -0,0 +1,17 @@
+import { Navbar, Nav, NavLink } from 'react-bootstrap';
+
+const TopNavBar = (props) => (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#vincerickey">Vince Rickey</Navbar.Brand>
+      <Nav classname="sections">
+        <NavLink href="#wsj">WSJ</NavLink>
+        <NavLink href="opensource">Open Source</NavLink>
+        <NavLink href="#wsj">Foreign Languages</NavLink>
+      </Nav>
+      <Nav>
+        <NavLink href="#resume">Resume</NavLink>
+      </Nav>
+    </Navbar>
+  );
+
+  export default TopNavBar
