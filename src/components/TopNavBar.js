88b4db61a3f6791a5506da675f4f5650292f5277
Vince Rickey <vince.rickey@gmail.com>
2020-11-29 18:56:53 -0500

    Forgot to save

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -1,14 +1,15 @@
 import { Navbar, Nav, NavLink } from 'react-bootstrap';
+import strings from '../strings';
 
 const TopNavBar = (props) => (
     <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="#vincerickey">Vince Rickey</Navbar.Brand>
+      <Navbar.Brand href="#vincerickey">{strings.NAME}</Navbar.Brand>
       <Nav classname="sections">
-        <NavLink href="#wsj">WSJ</NavLink>
-        <NavLink href="#opensource">Open Source</NavLink>
+        <NavLink href="#wsj">{strings.WSJ}</NavLink>
+        <NavLink href="#opensource">{strings.OPEN_SOURCE}</NavLink>
       </Nav>
       <Nav>
-        <NavLink href={props.resumeUrl}>Resume</NavLink>
+        <NavLink href={props.resumeUrl}>{strings.RESUME}</NavLink>
       </Nav>
     </Navbar>
   );
