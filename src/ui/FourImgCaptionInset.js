2b8c0b65866be17ab3e67007d5af96e9bddcf825
Vince Rickey <vince.rickey@gmail.com>
2026-01-20 23:56:28 -0500

    Use md=4 in FourImgCaptionInset to avoid clipping in md window sizes.

--- src/ui/FourImgCaptionInset.js
+++ src/ui/FourImgCaptionInset.js
@@ -4,19 +4,19 @@ import { Col, Row, Image } from 'react-bootstrap';
 function FourImgCaptionInset(props) {
   return (
     <Row>
-        <Col xs={12} sm={6}>
+        <Col xs={12} sm={6} md={3}>
             <Image className="object-fit-scale-down" src={props.imgOneSrc} rounded/>
             <h4 className="text-center">{props.captionOne}</h4>
         </Col>
-        <Col xs={12} sm={6}>
+        <Col xs={12} sm={6} md={3}>
             <Image className="object-fit-scale-down"  src={props.imgTwoSrc} rounded/>
             <h4 className="text-center">{props.captionTwo}</h4>
         </Col>
-        <Col xs={12} sm={6}>
+        <Col xs={12} sm={6} md={3}>
             <Image className="object-fit-scale-down"  src={props.imgThreeSrc} rounded/>
             <h4 className="text-center">{props.captionThree}</h4>
         </Col>
-        <Col xs={12} sm={6}>
+        <Col xs={12} sm={6} md={3}>
             <Image className="object-fit-scale-down"  src={props.imgFourSrc} rounded/>
             <h4 className="text-center">{props.captionFour}</h4>
         </Col>
